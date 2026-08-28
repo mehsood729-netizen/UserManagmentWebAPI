57f220ba6120ff8011069713b8ddf5c6d8eae0ca
Administrator <Administrator@DESKTOP-71ON71H>
2026-08-28 13:18:49 -0700

    Authentication service Complete

---
+++ UserManagmentWebAPI/UserManagmentWebAPI/Extensions/Mappers/UserRegisterDTOToUser.cs
@@ -0,0 +1,20 @@
+﻿using UserManagmentWebAPI.Data.Entities;
+using UserManagmentWebAPI.DTO_s;
+
+namespace UserManagmentWebAPI.Extensions.Mappers
+{
+    public static class UserRegisterDTOToUser
+    {
+        public static User Map(this UserRegisterDTO userRegisterDTO)
+        {
+            return new User
+            {
+
+                UserId = Guid.NewGuid(),
+                Contact = userRegisterDTO.Contact,
+                Email = userRegisterDTO.Email,
+                UserName = userRegisterDTO.UserName,
+            };
+        }
+    }
+}
